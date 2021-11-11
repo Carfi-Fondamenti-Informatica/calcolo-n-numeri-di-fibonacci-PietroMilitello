133943da056ec455351eda1298e8b13d543dbf2b
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-11-11 22:17:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0,a=0, b=1, c=0;
+    cin >> n;
+    if (n>=2){
+        cout <<"1"<< endl;
+        for (int i=1;i<n;i++){
+            c=a+b;
+            cout << c << endl;
+            a=b;
+            b=c;
+        }
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
